71f370a851a65f555135e91518e85a626a3451ab
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-21 22:44:50 +0530

    Created POST handler for Visitor creation

--- management/views.py
+++ management/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from management.serializers import CreateVisitor
 # Create your views here.
+
+class CreateUserView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self,request):
+        visitor = request.data.get('visitor')
+        if not visitor:
+            return Response({'response' : 'error', 'message' : 'No data found'})
+        serializer = CreateVisitor(data = visitor)
+        if serializer.is_valid():
+            saved_user = serializer.save()
+        else:
+            return Response({"response" : "error", "message" : serializer.errors})
+        return Response({"response" : "success", "message" : "Visitor added successfully"})
