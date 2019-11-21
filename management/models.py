ee78fbcea2fd55ac010897b88da55c8d96b81d8a
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-21 23:32:35 +0530

    Added related_names, corrected typos

--- management/models.py
+++ management/models.py
@@ -4,8 +4,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Host(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    phone = models.CharField(max_length = 12)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name="host")
+    phone = models.CharField(max_length = 13)
     ADDRESS_CHOICES = [
         ("Noida HQ", "<random_address1>"),
         ("Bangalore","<random_address2>")
@@ -18,18 +18,18 @@ class Host(models.Model):
     available = models.BooleanField(default = True)
 
     def __str__(self):
-        return user.username
+        return self.user.username
         
 class Visitor(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.EmailField()
-    phone = models.CharField(max_length=12)
+    phone = models.CharField(max_length=13)
     check_in_time = models.DateTimeField(auto_now_add=True, blank = True)
     check_out_time = models.TimeField()
-    host = models.OneToOneField(Host, on_delete = models.CASCADE, related_name="host")
+    host = models.ForeignKey(Host, on_delete = models.CASCADE, related_name="visitors")
 
     def __str__(self):
-        return email
+        return self.email
 
 @receiver(post_save, sender = User)
 def create_host(sender, instance, created, **kwargs):
@@ -38,4 +38,4 @@ def create_host(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender = User)
 def save_user_profile(sender, instance , **kwargs):
-    instance.profile.save()
+    instance.host.save()
