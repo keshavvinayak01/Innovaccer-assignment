d7bbb95a380f5b5987feb0ca6607e87fbfd5899c
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-21 21:34:34 +0530

    Initialised Django Models

--- management/models.py
+++ management/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Host(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phone = models.CharField(max_length = 12)
+    available = models.BooleanField(default = True)
+
+class Visitor(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    check_in_time = models.DateTimeField(auto_now_add=True, blank = True)
+    check_out_time = models.DateTimeField()
+    host = models.OneToOneField(Host, on_delete = models.CASCADE, related_name="host")
+
+@receiver(post_save, sender = User)
+def create_host(sender, instance, created, **kwargs):
+    if created:
+        Host.objects.create(user = instance, phone = "000000000")
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance , **kwargs):
+    instance.profile.save()
