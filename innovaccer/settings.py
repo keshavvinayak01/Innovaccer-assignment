3235693ce9952c737ffe940b28b2b51bc6b19acc
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-21 23:58:38 +0530

    Added Google SMTP for sending emails

--- innovaccer/settings.py
+++ innovaccer/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from global_config import get_global_config
+active_config = get_global_config()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,3 +132,11 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELITST = (
     'http://localhost:3000'
 )
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = active_config['EMAIL_ADDRESS']
+EMAIL_HOST_PASSWORD = active_config['PASSWORD']
