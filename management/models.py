200a7b69f1e63c1117a25b263360380bb15fccc1
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-21 22:42:21 +0530

    added __str__ methods to models

--- management/models.py
+++ management/models.py
@@ -17,6 +17,9 @@ class Host(models.Model):
     )
     available = models.BooleanField(default = True)
 
+    def __str__(self):
+        return user.username
+        
 class Visitor(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -24,6 +27,9 @@ class Visitor(models.Model):
     check_in_time = models.DateTimeField(auto_now_add=True, blank = True)
     check_out_time = models.TimeField()
     host = models.OneToOneField(Host, on_delete = models.CASCADE, related_name="host")
+
+    def __str__(self):
+        return email
 
 @receiver(post_save, sender = User)
 def create_host(sender, instance, created, **kwargs):
