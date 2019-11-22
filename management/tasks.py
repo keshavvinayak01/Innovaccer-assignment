ca88968c806e1dfaa39fb8b50b4da89a3618af4e
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-22 14:54:58 +0530

    Corrected mailing function

--- management/tasks.py
+++ management/tasks.py
@@ -3,11 +3,15 @@ from celery.utils.log import get_task_logger
 from datetime import datetime, timedelta
 from django.core.mail import send_mail
 from django.conf import settings
-
+from json import loads
+from django.contrib.auth.models import User
 logger = get_task_logger(__name__)
 
 @task(name = "send_email")
 def send_email(visitor, host, recipient):
+    visitor = loads(visitor)[0]['fields']
+    host = loads(host)[0]['fields']
+    host_user = User.objects.get(pk = host['user'])
     if recipient == "Host" :
         message = """
         You have a new visitor ! here are the details : 
@@ -20,16 +24,16 @@ def send_email(visitor, host, recipient):
         Host name : {}
         Address visited : {}
         """.format(
-            visitor.full_name,
-            visitor.email,
-            visitor.phone,
-            visitor.check_in_time,
-            visitor.check_out_time,
-            host.user.first_name + ' ' + host.user.last_name,
-            host.address
+            visitor['full_name'],
+            visitor['email'],
+            visitor['phone'],
+            visitor['check_in_time'],
+            visitor['check_out_time'],
+            host_user.first_name + ' ' + host_user.last_name,
+            host['address']
         )
         subject = "You have a new visitor!"
-        mail_to = [host.email]
+        mail_to = [host_user.email]
     else :
         message = """
         Thanks for visiting us! Here are your visit details : 
@@ -41,15 +45,15 @@ def send_email(visitor, host, recipient):
         Your Host : {}
         Address visited : {}
         """.format(
-            visitor.full_name,
-            visitor.phone,
-            visitor.check_in_time,
-            visitor.check_out_time,
-            host.user.first_name + ' ' + host.user.last_name,
-            host.address
+            visitor['full_name'],
+            visitor['phone'],
+            visitor['check_in_time'],
+            visitor['check_out_time'],
+            host_user.first_name + ' ' + host_user.last_name,
+            host['address']
         )
         subject = "Your Visit with us"
-        mail_to = [visitor.email]
+        mail_to = [visitor['email']]
 
     send_mail(
         subject,
